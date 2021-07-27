a9c13fbd1d17e3b44837bca5ce552f37f182beee
Phil Birnie <phil@7thstreetweb.com>
2021-07-27 13:55:27 -0400

    Adds CountRecords functionality.

---
+++ src/com/zoho/crm/api/record/CountWrapper.php
@@ -0,0 +1,28 @@
+<?php
+namespace com\zoho\crm\api\record;
+
+use com\zoho\crm\api\util\Model;
+
+class CountWrapper implements Model, CountHandler
+{
+
+	private  $count;
+
+	/**
+	 * The method to get the data
+	 * @return int A array representing the data
+	 */
+	public  function getCount()
+	{
+		return $this->count;
+	}
+
+	/**
+	 * The method to set the count
+	 * @param int $count Count
+	 */
+	public  function setCount(int $count)
+	{
+		$this->count=$count;
+	}
+} 
